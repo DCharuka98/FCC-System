9986f1ff94f5607813c14f060c38eff1c50bfc95
Dilshan Charuka <dcharuka98@gmail.com>
2026-01-10 19:07:17 +0530

    Database connection completed

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "127.0.0.1";
+$username   = "fcc_user";
+$password   = "fcc123";
+$dbname     = "fcc_db";
+$port       = 3307;
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+echo "Database connected successfully!";
+?>
